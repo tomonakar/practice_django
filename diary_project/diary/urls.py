dc6ae2f9483cad8dc791c2b0e8d450ee4f41c681
tomonakar <tomohisa.nak@gmail.com>
2019-05-27 08:02:17 +0900

    feat: form detail

--- diary_project/diary/urls.py
+++ diary_project/diary/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('add/', views.add, name='add'),  # /diary/add
     path('update/<int:pk>/', views.update, name='update'),  # /diary/update
     path('delete/<int:pk>', views.delete, name='delete'),  # /diary/delete
+    path('detail/<int:pk>', views.detail, name='detail'),  # /diary/detail
 ]
--- diary_project/diary/views.py
+++ diary_project/diary/views.py
@@ -52,3 +52,12 @@ def delete(request, pk):
         'day': day
     }
     return render(request, 'diary/day_confirm_delete.html', context)
+
+
+def detail(request, pk):
+    day = get_object_or_404(Day, pk=pk)
+
+    context = {
+        'day': day
+    }
+    return render(request, 'diary/day_detail.html', context)
