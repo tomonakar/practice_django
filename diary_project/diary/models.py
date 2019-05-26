eb14dca55347d3074a05f3785bcbabb57854e2fb
tomonakar <tomohisa.nak@gmail.com>
2019-05-26 22:45:58 +0900

    feat: initial migration

--- diary_project/diary/models.py
+++ diary_project/diary/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Day(models.Model):
+    title = models.CharField("タイトル", max_length=200)
+    text = models.TextField("本文")
+    date = models.DateTimeField("日付", default=timezone.now)
