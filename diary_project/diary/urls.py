eb2472214ff87e18dc5c3aafb9a8734a55e1e40f
tomonakar <tomohisa.nak@gmail.com>
2019-05-27 07:55:24 +0900

    feat: form delete

--- diary_project/diary/urls.py
+++ diary_project/diary/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),  # /diary
     path('add/', views.add, name='add'),  # /diary/add
     path('update/<int:pk>/', views.update, name='update'),  # /diary/update
+    path('delete/<int:pk>', views.delete, name='delete'),  # /diary/delete
 ]
--- diary_project/diary/views.py
+++ diary_project/diary/views.py
@@ -39,3 +39,16 @@ def update(request, pk):
         'form': form
     }
     return render(request, 'diary/day_form.html', context)
+
+
+def delete(request, pk):
+    day = get_object_or_404(Day, pk=pk)
+
+    if request.method == 'POST':
+        day.delete()
+        return redirect('diary:index')
+
+    context = {
+        'day': day
+    }
+    return render(request, 'diary/day_confirm_delete.html', context)
