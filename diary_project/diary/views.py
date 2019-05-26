587626b155b11f181cc6f18c4efbbbd37b7bf6a1
tomonakar <tomohisa.nak@gmail.com>
2019-05-27 08:49:47 +0900

    feat: paging

--- diary_project/diary/views.py
+++ diary_project/diary/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import DayCreateForm
@@ -7,6 +6,7 @@ from .models import Day
 
 class IndexView(generic.ListView):
     model = Day
+    paginate_by = 3
 
 
 class AddView(generic.CreateView):
