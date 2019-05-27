6a5347b4093cd56fd6de8ce7c297375918847abd
tomonakar <tomohisa.nak@gmail.com>
2019-05-27 23:56:02 +0900

    feat: モデル作成 feat: 管理画面作成 feat: リストViewを作成

--- employee_management/employee/models.py
+++ employee_management/employee/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField('部署名', max_length=20)
+    create_at = models.DateTimeField('日付', default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    first_name = models.CharField('名', max_length=20)
+    last_name = models.CharField('姓', max_length=20)
+    email = models.EmailField('メールアドレス', blank=True)
+    department = models.ForeignKey(
+        Department, verbose_name='部署', on_delete=models.PROTECT)
+    created_at = models.DateTimeField('日付', default=timezone.now)
+
+    def __str__(self):
+        return '{0} {1} {2}'.format(
+            self.last_name, self.first_name, self.department
+        )
--- employee_management/employee/views.py
+++ employee_management/employee/views.py
@@ -1,6 +1,6 @@
 from django.views import generic
+from .models import Employee
 
 
-class IndexView(generic.TemplateView):
-    template_name = 'employee/employee_list.html'
-
+class IndexView(generic.ListView):
+    model = Employee
