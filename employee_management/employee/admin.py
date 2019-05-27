dfb94ccb096f645960e4d13b2f7963ff12bb3e6b
tomonakar <tomohisa.nak@gmail.com>
2019-05-28 08:08:20 +0900

    feat: modelにクラブを追加

--- employee_management/employee/admin.py
+++ employee_management/employee/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Department, Employee
+from .models import Department, Employee, Club
 
 
 admin.site.register(Department)
 admin.site.register(Employee)
+admin.site.register(Club)
--- employee_management/employee/models.py
+++ employee_management/employee/models.py
@@ -10,12 +10,24 @@ class Department(models.Model):
         return self.name
 
 
+class Club(models.Model):
+    name = models.CharField('部活名', max_length=20)
+    created_at = models.DateTimeField('日付', default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
 class Employee(models.Model):
     first_name = models.CharField('名', max_length=20)
     last_name = models.CharField('姓', max_length=20)
     email = models.EmailField('メールアドレス', blank=True)
     department = models.ForeignKey(
-        Department, verbose_name='部署', on_delete=models.PROTECT)
+        Department, verbose_name='部署', on_delete=models.PROTECT,
+    )
+    club = models.ManyToManyField(
+        Club, verbose_name='部活',
+    )
     created_at = models.DateTimeField('日付', default=timezone.now)
 
     def __str__(self):
