b654545f0fd68faaf1b2ff6c8908411d00661e3d
tomonakar <tomohisa.nak@gmail.com>
2019-05-31 16:42:00 +0900

    refactor: ViewSetによるViewの統合 refactor: SimpleRouterによるRouterの統合

--- django_drf_react_blog/posts/views.py
+++ django_drf_react_blog/posts/views.py
@@ -1,31 +1,42 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import viewsets
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 
 
-# ListCreateAPIView: read-write endpoint
-# @see https://www.django-rest-framework.org/api-guide/generic-views/#listcreateapiview
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-# RetrieveUpdateDestoryAPIView: allow read, update, delete
-# @see https://www.django-rest-framework.org/api-guide/generic-views/#retrieveupdatedestroyapiview
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class UserList(generics.ListCreateAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
+# # ListCreateAPIView: read-write endpoint
+# # @see https://www.django-rest-framework.org/api-guide/generic-views/#listcreateapiview
+# class PostList(.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# # RetrieveUpdateDestoryAPIView: allow read, update, delete
+# # @see https://www.django-rest-framework.org/api-guide/generic-views/#retrieveupdatedestroyapiview
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = (IsAuthorOrReadOnly,)
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+
+# class UserList(generics.ListCreateAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+
+
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
