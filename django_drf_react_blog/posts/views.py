794638254cf48e7be0bd5edd405e23276ca6fbd6
tomonakar <tomohisa.nak@gmail.com>
2019-05-31 14:17:32 +0900

    feat: DRF導入 feat: DRF設定 feat: ルーティング設定 feat: シリアライザー設定 feat: view設定

--- django_drf_react_blog/posts/views.py
+++ django_drf_react_blog/posts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer
+
+
+# ListCreateAPIView: read-write endpoint
+# @see https://www.django-rest-framework.org/api-guide/generic-views/#listcreateapiview
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+# RetrieveUpdateDestoryAPIView: allow read, update, delete
+# @see https://www.django-rest-framework.org/api-guide/generic-views/#retrieveupdatedestroyapiview
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
