268e1efd7ce987a283e07a6a1825ea14e8a16263
tomonakar <tomohisa.nak@gmail.com>
2019-05-31 14:34:02 +0900

    feat: アクセス権限をViewに追加

--- django_drf_react_blog/posts/views.py
+++ django_drf_react_blog/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
 from .serializers import PostSerializer
@@ -7,6 +7,7 @@ from .serializers import PostSerializer
 # ListCreateAPIView: read-write endpoint
 # @see https://www.django-rest-framework.org/api-guide/generic-views/#listcreateapiview
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
@@ -14,5 +15,6 @@ class PostList(generics.ListCreateAPIView):
 # RetrieveUpdateDestoryAPIView: allow read, update, delete
 # @see https://www.django-rest-framework.org/api-guide/generic-views/#retrieveupdatedestroyapiview
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
